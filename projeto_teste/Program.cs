6124857dc20128e31439fd712973883db27b7e0d
Paulotrombinis <paulo.trombinis@gmail.com>
2020-10-31 14:54:40 -0300

    adicionei mensagem de bom dia

--- projeto_teste/Program.cs
+++ projeto_teste/Program.cs
@@ -11,6 +11,7 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("ola mundo");
+            Console.WriteLine("bom dia");
             Console.ReadLine();
         }
     }
