8e44511c5adc45928f2cc0f2e731ada64185f895
Paulotrombinis <paulo.trombinis@gmail.com>
2020-11-01 20:26:57 -0300

    outra msg

--- projeto_teste/Program.cs
+++ projeto_teste/Program.cs
@@ -13,6 +13,7 @@ namespace projeto_teste
             Console.WriteLine("ola mundo");
             Console.WriteLine("bom dia");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tchau!");
             Console.ReadLine();
         }
     }
